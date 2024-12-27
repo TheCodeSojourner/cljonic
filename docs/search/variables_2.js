bcd8fc2699d9b58df89c1534dafc224c0c3e5f0c
Paul Whittington <pwhittington@ricelake.com>
2024-12-27 10:30:29 -0700

    add Compose docs

---
+++ docs/search/variables_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['value_0',['value',['../structeach__function__is__invocable__with__next__return__type_1_1_are_invocable_with_return_3_011b6e34b7ffe9846fc8fb15c21463c9d0.html#a9d36ad3159ff082f9d0600fb10c9ec4c',1,'each_function_is_invocable_with_next_return_type::AreInvocableWithReturn&lt; F1, F2, Fs... &gt;::value'],['../structeach__function__is__invocable__with__next__return__type_1_1_are_invocable_with_return_3_01_f_01_4.html#a9f371efe762efb7390d6ad04a0c326ad',1,'each_function_is_invocable_with_next_return_type::AreInvocableWithReturn&lt; F &gt;::value']]]
+];
