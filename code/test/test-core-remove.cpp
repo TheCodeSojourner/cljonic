1bd5628b719046b9b94d7c11acdb81089be196b9
Paul Whittington <pwhittington@ricelake.com>
2025-01-10 12:48:34 -0700

    add Remove thus addressing issue #67

--- code/test/test-core-remove.cpp
+++ code/test/test-core-remove.cpp
@@ -5,27 +5,28 @@
 #include "cljonic-set.hpp"
 #include "cljonic-string.hpp"
 #include "cljonic-core-equal.hpp"
-#include "cljonic-core-filter.hpp"
+#include "cljonic-core-remove.hpp"
 
 using namespace cljonic;
 using namespace cljonic::core;
 
 constexpr auto Even = [](const int i) { return (0 == (i % 2)); };
 
-SCENARIO("Filter", "[CljonicCoreFilter]")
+SCENARIO("Remove", "[CljonicCoreRemove]")
 {
+    constexpr auto Even = [](const int i) { return (0 == (i % 2)); };
 
     constexpr auto a{Array<int, 10>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}};
-    CHECK(Equal(Array{0, 2, 4, 6, 8}, Filter(Even, a)));
+    CHECK(Equal(Array{1, 3, 5, 7, 9}, Remove(Even, a)));
 
     constexpr auto rng{Range<10>{}};
-    CHECK(Equal(Array{0, 2, 4, 6, 8}, Filter(Even, rng)));
+    CHECK(Equal(Array{1, 3, 5, 7, 9}, Remove(Even, rng)));
 
     constexpr auto rpt{Repeat<10, int>{1}};
-    CHECK(Equal(Array<int, 0>{}, Filter(Even, rpt)));
+    CHECK(Equal(Array{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, Remove(Even, rpt)));
 
     constexpr auto s{Set<int, 4>{1, 2, 3, 4, 5, 6}};
-    CHECK(Equal(Array{2, 4}, Filter(Even, s)));
+    CHECK(Equal(Array{1, 3}, Remove(Even, s)));
 
-    CHECK(Equal(Array{'l', 'l'}, Filter([](const char c) { return ('l' == c); }, String{"Hello"})));
+    CHECK(Equal(Array{'H', 'e', 'o'}, Remove([](const char c) { return ('l' == c); }, String{"Hello"})));
 }
